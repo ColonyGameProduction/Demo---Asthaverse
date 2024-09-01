72baf3fd8e8f3f012f209475fcd9443d0ae96c42
ElshadRyan <117740569+ElshadRyan@users.noreply.github.com>
2024-09-01 22:24:11 +0700

    Join

---
+++ Assets/Script/Kodingan Elshad/PlayerAction.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;  
+
+//kelas untuk player action seperti attacking, scope, dan Silent kill
+
+public class PlayerAction : ExecuteLogic
+{
+    public PlayerActionInput inputActions;
+
+    //supaya input action bisa digunakan
+    private void Awake()
+    {
+        inputActions = new PlayerActionInput();
+        inputActions.InputPlayerAction.Shooting.Enable();
+        inputActions.InputPlayerAction.SilentKill.Enable();
+        inputActions.InputPlayerAction.ChangingWeapon.Enable();
+    }
+
+    private void Start()
+    {
+        //membuat event untuk menjalankan aksi yang dipakai oleh player
+        inputActions.InputPlayerAction.Shooting.performed += Shooting_Performed;
+        inputActions.InputPlayerAction.SilentKill.performed += SilentKill_performed;
+        inputActions.InputPlayerAction.ChangingWeapon.performed += ChangingWeapon_performed;
+    }
+
+    private void ChangingWeapon_performed(InputAction.CallbackContext context)
+    {
+        ChangingWeapon();
+    }
+
+    //event ketika 'SilentKill' dilakukan
+    private void SilentKill_performed(InputAction.CallbackContext context)
+    {
+        SilentKill();
+    }
+
+
+    //event ketika 'Shoot' dilakukan
+    private void Shooting_Performed(InputAction.CallbackContext context)
+    {
+        Shoot();        
+    }    
+
+    
+    
+}
