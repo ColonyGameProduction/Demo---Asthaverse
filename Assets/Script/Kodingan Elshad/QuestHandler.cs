8bc0fdbc8b100ace1425fa0a4948358ba246cb13
ElshadRyan <elshadpw@gmail.com>
2024-12-20 17:15:23 +0700

    update

---
+++ Assets/Script/Kodingan Elshad/QuestHandler.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestHandler : MonoBehaviour
+{
+    public bool eavesdropComplete;
+    public bool keyItemInteracted;
+}
